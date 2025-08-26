e56a51125a817e84873da0ef8d3310875a560a98
Waroon Ragwongsiri <waragwon@z2t10c2.42bangkok.com>
2025-08-26 13:22:58 +0700

    strncmp fix unsigned char

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -28,10 +28,10 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	while (s1[i] == s2[i] && s1[i] && s2[i] && i < (n - 1))
 	{
 		if (s1[i] != s2[i])
-			return (s1[i] - s2[i]);
+			return (((unsigned char) s1[i]) - ((unsigned char) s2[i]));
 		i++;
 	}
-	return (s1[i] - s2[i]);
+	return (((unsigned char) s1[i]) - ((unsigned char) s2[i]));
 }
 
 // #include <stdio.h>
