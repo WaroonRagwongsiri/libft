e377cee3f2a20d85a2f3c5b6954677737cc67dd9
Waroon Ragwongsiri <waragwon@z2t9c1.42bangkok.com>
2025-10-31 15:26:21 +0700

    fix

--- srcs/garbage.c
+++ srcs/garbage.c
@@ -6,7 +6,7 @@
 /*   By: waragwon <waragwon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/30 10:09:54 by waragwon          #+#    #+#             */
-/*   Updated: 2025/10/31 13:18:46 by waragwon         ###   ########.fr       */
+/*   Updated: 2025/10/31 15:24:29 by waragwon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,14 +25,11 @@ void	*ft_safe_calloc(size_t nmemb, size_t size, char *cmd)
 	}
 	new = ft_lstnew(NULL);
 	if (!new)
-	{
-		ft_lstclear(&mem_lst, free);
 		return (NULL);
-	}
 	ft_lstadd_back(&mem_lst, new);
 	ptr = ft_calloc(nmemb, size);
 	if (!ptr)
-		ft_lstclear(&mem_lst, free);
+		return (NULL);
 	new->content = ptr;
 	return (ptr);
 }
